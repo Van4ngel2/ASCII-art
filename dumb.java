d72cfd6babdea28a68dfcf91d68e28e725ff07be
Van4ngel2 <110430887+Van4ngel2@users.noreply.github.com>
2023-03-16 23:14:34 -0300

    v1

---
+++ dumb.java
@@ -0,0 +1,34 @@
+public class dumb {
+
+    public static void main(String[] args) {
+
+        System.out.println(".");
+        System.out.println("e$$$c");
+        System.out.println("$$$$$$b");
+        System.out.println("d$$$$$$$b.");
+        System.out.println("J$$$$$$$$$e.");
+        System.out.println("?$$$$$$$$$$$bu");
+        System.out.println("?$$$$$$$$$$$$$$$e,");
+        System.out.println("$$$$$$$$$$$$$$$$$$$eu.");
+        System.out.println("?$$$$$$$$$$$$$$$$$$$$$$$eu.");
+        System.out.println("?$$$$$$$$$$$$$$$$$$$$$$$e`''=u");
+        System.out.println(" ?$$$$$$$$$$$$$$$$$$$$$$$$$bc'?c.");
+        System.out.println("  ?$$$$$$$$$$$$$$$$$$$$$$$$$$$e.?b.");
+        System.out.println("   ?$$$$$$$$$$$$$$$$$$$$$$$$$$$$b ?b");
+        System.out.println("    `?$$$$$$$$$$$$$$$$$$$$$$$$$$$$.?$      ,;");
+        System.out.println("         z$bc`'?$$$$$$$$$$$$$$$$$$$$$$$??R$e,`");
+        System.out.println("        d$$$$$$beu,'???$$$$$$$P'$$$$$$$$$$$$$$'-eeeeu.");
+        System.out.println("       d$$$$$$$$$$$$ih,CCcCC-.e$$$$$$$$P'u$$$$$ eeeu`??c");
+        System.out.println(" ,zdF,d$$$$$$$$$$$$$$ih,CCCC<$$$$$$$$$F .$$P$?? $$$$$$e`?,");
+        System.out.println(" f$3$$$$$$$$$$$$$$$$$$>$CCCC`$$$$$$$$$$ed$ee=. 3$$$$$$$$c'e");
+        System.out.println(" `??''''''   '?$$$$$$$:FCCCCC`$$$$$$'')3$$$$$$eu,`'???$$$$d$.");
+        System.out.println("                '??$$F<$CCCCCC,`?P$$$  `'??$$$$$$$$???e`?$$$$b.     ud$b");
+        System.out.println("                       ???$$edd$.c,`?c,^,$$b= rafadd$k`'' ?$$$$$eu e$$$$$");
+        System.out.println("                           ;7??I?   ;,`'' -e$$$$$$$$$$$$$$$$$$$$$$eeCC3'");
+        System.out.println("                           `?$$$$$  ,$$$$, `?$$$$$$$$$$$$$$$$$$$$$$$P");
+        System.out.println("                             `'''  '?????'    `??$$$$$$$$$$$$$$$PF'");
+        System.out.println("                             '''''''''''''''''");
+
+        System.out.println("Dumbo <3");
+    }
+}
---
+++ smiley.java
@@ -0,0 +1,16 @@
+public class smiley {
+
+    public static void main(String[] args) {
+
+        System.out.println("####################");
+        System.out.println("###'#`#####'#`######");
+        System.out.println("###bod#####bod######");
+        System.out.println("########'`##########");
+        System.out.println("########oo##########");
+        System.out.println("#####P`######'`#####");
+        System.out.println("####b '''''' d######");
+        System.out.println("#####booooood#######");
+
+    }
+
+}
